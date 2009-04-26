8a5e9eb9438ca5baaa62ac3b4dabc8e2831845c1
marco-tijuana <marco-tijuana@00aac72d-264a-0410-add3-e1605ea6af76>
2009-04-26 16:08:11 +0000

    Moving script to it's place..

--- scripts/local/run_example.sh
+++ scripts/local/run_example.sh
@@ -16,11 +16,14 @@ echo "Starting the acceptors"
 xterm -geometry 80x24+10+10 -e "cd $PROJ_DIR; ./example_acceptor 0; $KEEP_XTERM_OPEN" &
 xterm -geometry 80x24+400+10 -e "cd $PROJ_DIR; ./example_acceptor 1; $KEEP_XTERM_OPEN" &
 xterm -geometry 80x24+800+10 -e "cd $PROJ_DIR; ./example_acceptor 2; $KEEP_XTERM_OPEN" &
-sleep 5;
+
+xterm -geometry 80x24+600+300 -e "cd $PROJ_DIR; ./example_learner; $KEEP_XTERM_OPEN" &
+sleep 3;
 
 xterm -geometry 80x24+10+300 -e "cd $PROJ_DIR; ./example_proposer 0; $KEEP_XTERM_OPEN" &
 
+xterm -geometry 80x8+10+600 -e "cd $PROJ_DIR; ./benchmark_client; $KEEP_XTERM_OPEN" &
 
 echo "Press enter to send the kill signal"
 read
-killall -INT example_acceptor example_proposer example_learner
+killall -INT example_acceptor example_proposer example_learner benchmark_client
